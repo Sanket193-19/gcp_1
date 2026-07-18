acf7c1cc784c835a0b8f09a65fbe16d05a13a99d
sanket <sanketmohite3333@gmail.com>
2026-07-18 19:55:36 +0530

    change

--- data/BQ/gold.sql
+++ data/BQ/gold.sql
@@ -1,50 +1,22 @@
-CREATE TABLE IF NOT EXISTS `project-4dd17051-6b0d-4014-b11.gold_dataset.patient_history` (
-    Patient_Key STRING,
-    FirstName STRING,
-    LastName STRING,
-    Gender STRING,
-    DOB DATE,
-    Address STRING,
-    EncounterDate DATE,
-    EncounterType STRING,
-    Transaction_Key STRING,
-    VisitDate DATE,
-    ServiceDate DATE,
-    BilledAmount FLOAT64,
-    PaidAmount FLOAT64,
-    ClaimStatus STRING,
-    ClaimAmount STRING,
-    ClaimPaidAmount STRING,
-    PayorType STRING
+CREATE TABLE IF NOT EXISTS `project-4dd17051-6b0d-4014-b11.gold_dataset.provider_charge_summary` (
+    Provider_Name STRING,
+    Dept_Name STRING,
+    Amount FLOAT64
 );
 
+# truncate table
+TRUNCATE TABLE `project-4dd17051-6b0d-4014-b11.gold_dataset.provider_charge_summary`;
 
-# TRUNCATE TABLE
-TRUNCATE TABLE `project-4dd17051-6b0d-4014-b11.gold_dataset.patient_history`;
-INSERT INTO `project-4dd17051-6b0d-4014-b11.gold_dataset.patient_history`
+# insert data
+INSERT INTO `project-4dd17051-6b0d-4014-b11.gold_dataset.provider_charge_summary`
 SELECT 
-    p.Patient_Key,
-    p.FirstName,
-    p.LastName,
-    p.Gender,
-    p.DOB,
-    p.Address,
-    e.EncounterDate,
-    e.EncounterType,
-    t.Transaction_Key,
-    t.VisitDate,
-    t.ServiceDate,
-    t.Amount AS BilledAmount,
-    t.PaidAmount,
-    c.ClaimStatus,
-    c.ClaimAmount,
-    c.PaidAmount AS ClaimPaidAmount,
-    c.PayorType
-FROM `project-4dd17051-6b0d-4014-b11.silver_dataset.patients` p
-LEFT JOIN `project-4dd17051-6b0d-4014-b11.silver_dataset.encounters` e 
-    ON SPLIT(p.Patient_Key, '-')[OFFSET(0)] || '-' || SPLIT(p.Patient_Key, '-')[OFFSET(1)] = e.PatientID
-LEFT JOIN `project-4dd17051-6b0d-4014-b11.silver_dataset.transactions` t 
-    ON SPLIT(p.Patient_Key, '-')[OFFSET(0)] || '-' || SPLIT(p.Patient_Key, '-')[OFFSET(1)] = t.PatientID
-LEFT JOIN `project-4dd17051-6b0d-4014-b11.silver_dataset.claims` c 
-    ON t.SRC_TransactionID = c.TransactionID
-WHERE p.is_current = TRUE;
+    CONCAT(p.firstname, ' ', p.LastName) AS Provider_Name,
+    d.Name AS Dept_Name,
+    SUM(t.Amount) AS Amount
+FROM `project-4dd17051-6b0d-4014-b11.silver_dataset.transactions` t
+LEFT JOIN `project-4dd17051-6b0d-4014-b11.silver_dataset.providers` p 
+    ON SPLIT(p.ProviderID, "-")[SAFE_OFFSET(1)] = t.ProviderID
+LEFT JOIN `project-4dd17051-6b0d-4014-b11.silver_dataset.departments` d 
+    ON SPLIT(d.Dept_Id, "-")[SAFE_OFFSET(0)] = p.DeptID
+WHERE t.is_quarantined = FALSE AND d.Name IS NOT NULL
+GROUP BY Provider_Name, Dept_Name;
