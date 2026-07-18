07773ff5c6c710cd8f8c285809e763169e4738c9
sanket <sanketmohite3333@gmail.com>
2026-07-18 18:09:05 +0530

    chnage

--- dags/parent_dag.py
+++ dags/parent_dag.py
@@ -25,14 +25,8 @@ with DAG(
     default_args=ARGS,
     tags=["parent", "orchestration", "etl"]
 ) as dag:
-/*
-    # Task to trigger PySpark DAG
-    trigger_pyspark_dag = TriggerDagRunOperator(
-        task_id="trigger_pyspark_dag",
-        trigger_dag_id="pyspark_dag",
-        wait_for_completion=True,
-    )
-*/
+    
+
     # Task to trigger BigQuery DAG
     trigger_bigquery_dag = TriggerDagRunOperator(
         task_id="trigger_bigquery_dag",
